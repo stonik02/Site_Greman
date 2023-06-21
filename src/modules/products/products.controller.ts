1b02b7d48b9d6caabe2780e288b822a354387670
Denis <skitlsskitlovich@gmail.com>
2023-06-21 06:40:20 +0300

    Update Sale/Actual Products

--- src/modules/products/products.controller.ts
+++ src/modules/products/products.controller.ts
@@ -1,47 +1,67 @@
 import {
-  Body,
-  Controller,
-  Get,
-  Param,
-  Patch,
-  Post,
-  UploadedFile,
-  UseInterceptors,
-} from '@nestjs/common';
-import { ProductsService } from './products.service';
-import { CreateProductDto } from './dto';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { Product } from './models/product.model';
-import { UpdateProductDto } from './dto/updatedto';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
+	Body,
+	Controller,
+	Get,
+	Param,
+	Patch,
+	Post,
+	UploadedFile,
+	UseInterceptors,
+} from '@nestjs/common'
+import { ProductsService } from './products.service'
+import { CreateProductDto } from './dto'
+import { FileInterceptor } from '@nestjs/platform-express'
+import { Product } from './models/product.model'
+import { UpdateProductDto } from './dto/updatedto'
+import { ApiResponse, ApiTags } from '@nestjs/swagger'
 
 @ApiTags('products')
 @Controller('products')
 export class ProductsController {
-  constructor(private readonly productsService: ProductsService) {}
+	constructor(private readonly productsService: ProductsService) {}
 
-  @ApiResponse({status: 201, type: CreateProductDto})
-  @Post('')
-  @UseInterceptors(FileInterceptor('image'))
-  createProduct(@Body() dto: CreateProductDto,
-                 @UploadedFile() image
-                ) {
-    return this.productsService.create(dto, image)
+	@ApiResponse({ status: 201, type: CreateProductDto })
+	@Post('')
+	@UseInterceptors(FileInterceptor('image'))
+	createProduct(@Body() dto: CreateProductDto, @UploadedFile() image) {
+		return this.productsService.create(dto, image)
+	}
+
+	@ApiResponse({ status: 200, type: CreateProductDto })
+	@Get('')
+	getAllProducts(): Promise<Product[]> {
+		return this.productsService.getAll()
+	}
+
+	@ApiResponse({ status: 200, type: CreateProductDto })
+	@Patch('update/:id')
+	@UseInterceptors(FileInterceptor('image'))
+	updateProduct(
+		@Param('id') id,
+		@Body() dto: UpdateProductDto,
+		@UploadedFile() image,
+		@Body('size') sizesToAdd: number[],
+	) {
+		return this.productsService.update(dto, image, id, sizesToAdd)
+	}
+
+  @Post('update-sale')
+  updateSale(@Body('id') id: number[]) {
+    return this.productsService.updateSale(id)
   }
 
-  @ApiResponse({status: 200, type: CreateProductDto})
-  @Get('')
-  getAllProducts():Promise<Product[]> {
-    return this.productsService.getAll()
+  @Post('update-actual')
+  updateActual(@Body('id') id: number[]) {
+    return this.productsService.updateActual(id)
   }
 
-  @ApiResponse({status: 200, type: CreateProductDto})
-  @Patch('update/:id')
-  @UseInterceptors(FileInterceptor('image'))
-  updateProduct(@Param('id') id, @Body() dto: UpdateProductDto, @UploadedFile() image, @Body('size') sizesToAdd: number[]){
-    return this.productsService.update(dto, image, id, sizesToAdd)
+  @Get('sale')
+  getSaleProduct(): Promise<Product[]> {
+    return this.productsService.getSaleProduct()
   }
 
- 
-
+  @Get('actual')
+  getActualProduct(): Promise<Product[]> {
+    return this.productsService.getActualProduct()
+  }
 }
--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,60 +1,65 @@
 import {
-    Body,
-    Controller, Delete, Get, Patch, Post, Req, UseGuards,
- 
-  } from '@nestjs/common';
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Patch,
+	Post,
+	Req,
+	UseGuards,
+} from '@nestjs/common'
 
-import { UsersService } from './users.service';
-import { UpdateUserDTO } from './dto';
-import { ApiResponse, ApiTags } from '@nestjs/swagger';
-import { User } from './models/users.model';
-import { JWTAuthGuard } from '../auth/jwt-guard';
-import { Roles } from '../auth/has-roles.decorator';
-import { RolesGuard } from '../auth/roles.guard';
+import { UsersService } from './users.service'
+import { UpdateUserDTO } from './dto'
+import { ApiResponse, ApiTags } from '@nestjs/swagger'
+import { User } from './models/users.model'
+import { JWTAuthGuard } from '../auth/jwt-guard'
+import { Roles } from '../auth/has-roles.decorator'
+import { RolesGuard } from '../auth/roles.guard'
 
 @ApiTags('users')
 @Controller('users')
 export class UsersController {
-  constructor(private readonly userService: UsersService) {}
+	constructor(private readonly userService: UsersService) {}
 
+	@ApiResponse({ status: 200, type: UpdateUserDTO })
+	@UseGuards(JWTAuthGuard)
+	@Patch('update')
+	updateUser(
+		@Body() updateDto: UpdateUserDTO,
+		@Req() request,
+	): Promise<UpdateUserDTO> {
+		const user = request.user
+		return this.userService.updateUser(user.email, updateDto)
+	}
 
-  @ApiResponse({status: 200, type: UpdateUserDTO})
-  @UseGuards(JWTAuthGuard)
-  @Patch('update')
-  updateUser(@Body() updateDto: UpdateUserDTO, @Req() request): Promise<UpdateUserDTO> {
-    const user = request.user
-    return this.userService.updateUser(user.email, updateDto)
-  }
+	@Get('all')
+	allUsers(): Promise<User[]> {
+		return this.userService.allUser()
+	}
 
-  @Get('all')
-  allUsers(): Promise<User[]> {
-    return this.userService.allUser()
-  }
+	@ApiResponse({ status: 200, type: Boolean })
+	@UseGuards(JWTAuthGuard)
+	@Delete('me')
+	deleteUser(@Req() request) {
+		const user = request.user
+		return this.userService.deleteUser(user.email)
+	}
 
-  @ApiResponse({status: 200, type: Boolean})
-  @UseGuards(JWTAuthGuard)
-  @Delete('me')
-  deleteUser(@Req() request) {
-    const user = request.user
-    return this.userService.deleteUser(user.email)
-  }
+	@ApiResponse({ status: 200, type: Boolean })
+	@Roles('user')
+	@UseGuards(RolesGuard)
+	@Get('me')
+	async GetData(@Req() request): Promise<User> {
+		const user = await request.user
+		console.log(user.user)
+		return this.userService.GetUser(user.email)
+	}
 
-  @ApiResponse({status: 200, type: Boolean})
-  @Roles("user")
-  @UseGuards(RolesGuard)
-  @Get('me')
-  async GetData(@Req() request): Promise<User>  {
-    
-    const user = await request.user
-    console.log(user.user)
-    return this.userService.GetUser(user.email)
-  }
-
-  @ApiResponse({status: 200, type: UpdateUserDTO})
-  @UseGuards(JWTAuthGuard)
-  @Get('busket')
-  myBusket(@Req() req) {
-    return this.userService.myBusket(req.user.id)
-  }
-
+	@ApiResponse({ status: 200, type: UpdateUserDTO })
+	@UseGuards(JWTAuthGuard)
+	@Get('busket')
+	myBusket(@Req() req) {
+		return this.userService.myBusket(req.user.id)
+	}
 }
